2f6827ff863f4dca945ccc6bf952cc586ec822e9
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2026-01-01 23:21:51 +0200

    Add project files.

---
+++ Controllers/PredictionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using static Craciun_Adriana_Lab4.PricePredictionModel;
+using System.IO;
+
+namespace Craciun_Adriana_Lab4.Controllers
+{
+    public class PredictionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Price()
+        {
+            return View(new ModelInput());
+        }
+
+        [HttpPost]
+        public IActionResult Price(ModelInput input)
+        {
+            try
+            {
+                MLContext mlContext = new MLContext();
+                var modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PricePredictionModel.mlnet");
+                ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+                var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+                ModelOutput result = predEngine.Predict(input);
+                ViewBag.Price = result.Score;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = $"Error: {ex.Message}";
+            }
+            return View(input);
+        }
+    }
+}
