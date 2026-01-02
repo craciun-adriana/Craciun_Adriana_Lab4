24b88fe70ac5cec95efb8a0ac6a0db4e14992d2e
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2026-01-02 15:20:28 +0200

    update

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Craciun_Adriana_Lab4.Data
+{
+    public class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Craciun_Adriana_Lab4.Models.PredictionHistory> PredictionHistories { get; set; } = null!;
+    }
+}
